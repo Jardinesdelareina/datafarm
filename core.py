06c511afee629d7aee225faa12221188eafde8e3
jardinesdelareina <jardinesdelareina@mail.ru>
2023-02-15 21:58:26 +0300

    сохранение данных по каждой паре в отдельный файл csv в директории data

--- core.py
+++ core.py
@@ -1,7 +1,7 @@
 import websocket, json, threading, os
 import pandas as pd
 
-SYMBOL = ['xrpusdt', 'ethusdt', 'linkusdt']
+SYMBOL = ['btcusdt', 'adausdt', 'xrpusdt', 'ethusdt', 'linkusdt']
 SOCKETS = [f'wss://stream.binance.com:9443/ws/{symbol}@trade' for symbol in SYMBOL]
 
 def on_open(ws):
@@ -16,12 +16,12 @@ def on_message(ws, df):
     df.columns = ['Symbol', 'Time', 'Price']
     df.Time = pd.to_datetime(df.Time, unit='ms', utc=True, infer_datetime_format=True)
     df.Price = df.Price.astype(float)
-    with open('data.csv', 'a') as f:
-        if os.stat('data.csv').st_size == 0:
+    with open(f'data/{df.Symbol[0]}.csv', 'a') as f:
+        if os.stat(f'data/{df.Symbol[0]}.csv').st_size == 0:
             df.to_csv(f, mode='a', header=True, index=False)
         else:
             df.to_csv(f, mode='a', header=False, index=False)
-    print(df.Price)
+    print(df.Symbol, df.Price)
     return df
 
 def main(socket):
@@ -30,11 +30,10 @@ def main(socket):
         on_open=on_open, 
         on_close=on_close, 
         on_message=on_message)
-
     ws.run_forever()
 
 threads = []
 for socket in SOCKETS:
-    t = threading.Thread(target=main, args=(socket,))
-    threads.append(t)
-    t.start()
+    thread_socket = threading.Thread(target=main, args=(socket,))
+    threads.append(thread_socket)
+    thread_socket.start()

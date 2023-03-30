8c4fe5c2ad48f488e33a52ca6c17bb6b983c0104
jardinesdelareina <jardinesdelareina@mail.ru>
2023-03-31 02:33:00 +0300

    get_top_coin

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-import environs
+import environs, json
 from sqlalchemy import create_engine
+from binance.um_futures import UMFutures
 
 env = environs.Env()
 env.read_env('.env')
@@ -15,3 +16,10 @@ ENGINE = create_engine(f'postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{
 # Telegram API
 TELETOKEN = env('TELETOKEN')
 CHAT_ID = env('CHAT_ID')
+
+# Binance API
+CLIENT = UMFutures(env('API_KEY'), env('SECRET_KEY'))
+GENERAL_BALANCE = round(float(CLIENT.balance()[6]['balance']), 2)
+AVAILABLE_BALANCE = round(float(CLIENT.balance()[6]['availableBalance']), 2)
+INFO = CLIENT.exchange_info()
+#print(json.dumps(INFO, indent=4, sort_keys=True))

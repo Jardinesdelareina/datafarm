08ca892832c995aa6cb9c6bb06126fd330d3089a
jardinesdelareina <jardinesdelareina@mail.ru>
2023-04-03 02:34:38 +0300

    place_order

--- core.py
+++ core.py
@@ -1,22 +1,50 @@
-import asyncio, requests
+import asyncio, requests, json
 import pandas as pd
 from binance import BinanceSocketManager
-from config import CLIENT, ENGINE, TELETOKEN, CHAT_ID
+from binance.helpers import round_step_size
 from sqlalchemy import text
 from concurrent.futures import ThreadPoolExecutor
+from config import CLIENT, ENGINE, TELETOKEN, CHAT_ID
 
 symbol_list = [
-    'BTCUSDT', 'ETHUSDT'
+    'btcusdt', 'ethusdt', 'bnbusdt', 'xrpusdt', 'dotusdt', 'linkusdt', 'xtzusdt', 
+    'adausdt', 'solusdt', 'maticusdt', 'avaxusdt', 'uniusdt', 'trxusdt', 'xlmusdt',
+    'vetusdt', 'axsusdt', 'zilusdt', 'dogeusdt', 'nearusdt', 'aaveusdt', 'ltcusdt',
 ]
 
 
 class Datafarm:
+    """ Базовый класс, содержащий инфраструктуру обработки данных, торговую стратегию, 
+        построенную на основе полученных данных и логику взаимодействия с API Binance.
+    """
 
     def __init__(self, symbol):
+        """ Конструктор класса Datafarm
+
+            symbol (str): Название тикера, которое передается в атрибуте класса
+
+            __open_position (bool): Состояние, в котором находится алгоритм. 
+                                    Если нет открытой позиции, значение атрибута - False,
+                                    если произошло событие signal_buy - True
+
+            __last_signal (str): Текстовое сообщение о торговом сигнале, передаваемое как 
+                                уведомление в Telegram и в терминал.
+
+            __last_log (str): Текстовое уведомление, логирующее работу алгоритма в состоянии
+                            ожидания торгового сигнала.
+
+            ** __last_signal и __last_log служат для предотвращения дублирования уведомлений
+            при поступающих через вебсокеты идентичных данных.
+
+            running (bool): Состояние цикла while в методе main. 
+                            Если True - данные через API Binance непрерывно поступают,
+                            если False - поступление данных прекращается, работа алгоритма останавливается
+        """
         self.symbol = symbol
-        self.open_position = False
-        self.last_signal = None
-        self.last_log = None
+        self.__open_position = False
+        self.__last_signal = None
+        self.__last_log = None
+        self.running = True
 
 
     def send_message(self, message: str) -> str:
@@ -41,8 +69,65 @@ class Datafarm:
         return counter
 
     
+    def calculate_quantity(self) -> float:
+        """ Расчет объема ордера """
+        symbol_info = CLIENT.get_symbol_info(self.symbol)
+        step_size = symbol_info.get('filters')[1]['stepSize']
+        order_volume = self.qnty / self.last_price
+        order_volume = round_step_size(order_volume, step_size)
+        return order_volume
+
+
+    def place_order(self, order_side: str):
+        """ Размещение ордеров
+            
+            order_side (str): Направление ордера, передаваемое при вызове функции в алгоритме.
+        """
+
+        if order_side == 'BUY':
+            order = CLIENT.create_order(
+                symbol=self.symbol, 
+                side='BUY', 
+                type='MARKET', 
+                quantity=self.calculate_quantity(),
+            )
+            self.__open_position = True
+            self.buy_price = round(
+                float(order.get('fills')[0]['price']), 
+                self.round_float(num=self.last_price)
+            )
+            message = f'{self.symbol} \n Buy \n {self.buy_price}'
+            self.send_message(message)
+            print(message)
+            print(json.dumps(order, indent=4, sort_keys=True))
+
+        elif order_side == 'SELL':
+            order = CLIENT.create_order(
+                symbol=self.symbol, 
+                side='SELL', 
+                type='MARKET', 
+                quantity=self.calculate_quantity(),
+            )
+            self.open_position = False
+            self.sell_price = round(
+                float(order.get('fills')[0]['price']), 
+                self.round_float(num=self.last_price)
+            )
+            result = round((self.sell_price - self.buy_price * self.calculate_quantity()), 2)
+            message = f'{self.symbol} \n Sell \n {self.sell_price} \n Результат: {result} USDT'
+            self.send_message(message)
+            print(message)
+            print(json.dumps(order, indent=4, sort_keys=True))
+
+
     def get_data(self, stream):
-        """ Получение данных с биржи
+        """ Получение данных с биржи, сохранение в базу данных, чтение, обработка данных
+            и преобразование их в торговые сигналы
+
+            Структура таблицы базы данных:
+            symbol (str): Название тикера
+            time (datetime): Время поступления данных
+            price (float): Значение цены тикера
         """
         df = pd.DataFrame([stream])
         df = df.loc[:,['s', 'E', 'p']]
@@ -53,7 +138,7 @@ class Datafarm:
         df.to_sql(name=f'{db_ticker.lower()}', con=ENGINE, if_exists='append', index=False)
 
         def execute_query(query: str):
-            """ Оборачивание запросов к базе данных
+            """ Обработка запросов к базе данных.
 
                 query (str): SQL запрос
                 return (Pandas DataFrame): Результат выполнения запроса 
@@ -65,7 +150,7 @@ class Datafarm:
                 return value
 
         # Последняя цена тикера  
-        last_price = execute_query(
+        self.last_price = execute_query(
             f"SELECT price FROM {db_ticker} ORDER BY time DESC LIMIT 1"
         )
 
@@ -80,55 +165,70 @@ class Datafarm:
         )
 
         # Цена выше минимума за последний час на 1%
-        signal_buy = round((min_price_last_hour + (min_price_last_hour * 0.01)), self.round_float(num=last_price))
+        signal_buy = round(
+            (min_price_last_hour + (min_price_last_hour * 0.01)), 
+            self.round_float(num=self.last_price)
+        )
 
-        # Цена ниже максимума за последний час на 0.5%
-        signal_sell = round((max_price_last_hour - (max_price_last_hour * 0.01)), self.round_float(num=last_price))
+        # Цена ниже максимума за последний час на 1%
+        signal_sell = round(
+            (max_price_last_hour - (max_price_last_hour * 0.01)), 
+            self.round_float(num=self.last_price)
+        )
 
-        if not self.open_position:
-            if last_price > signal_buy:
-                message = f'{db_ticker.upper()}: {last_price} Buy'
-                if message != self.last_signal:
+        if not self.__open_position:
+            if self.last_price > signal_buy:
+                self.place_order('BUY')
+                message = f'{db_ticker.upper()}: {self.last_price} Buy'
+                if message != self.__last_signal:
                     self.send_message(message)
                     print(message)
-                    self.last_signal = message
-                self.open_position = True
+                    self.__last_signal = message
+                self.__open_position = True
             else:
-                message = f'{db_ticker.upper()}: {last_price} BUY: {signal_buy}'
-                if message != self.last_log:
-                    print(self.last_log)
-                    self.last_log = message
-        if self.open_position:
-            if last_price < signal_sell:
-                message = f'{db_ticker.upper()}: {last_price} Sell'
-                if message != self.last_signal:
+                message = f'{db_ticker.upper()}: {self.last_price} BUY: {signal_buy}'
+                if message != self.__last_log:
+                    print(self.__last_log)
+                    self.__last_log = message
+
+        if self.__open_position:
+            if self.last_price < signal_sell:
+                self.place_order('SELL')
+                message = f'{db_ticker.upper()}: {self.last_price} Sell'
+                if message != self.__last_signal:
                     self.send_message(message)
                     print(message)
-                    self.last_signal = message
-                self.open_position = False
+                    self.__last_signal = message
+                self.__open_position = False
             else:
-                message = f'{db_ticker.upper()}: {last_price} SELL: {signal_buy}'
-                if message != self.last_log:
-                    print(self.last_log)
-                    self.last_log = message
+                message = f'{db_ticker.upper()}: {self.last_price} SELL: {signal_buy}'
+                if message != self.__last_log:
+                    print(self.__last_log)
+                    self.__last_log = message
+
+
+    def stop(self):
+        """ Остановка выполнения программы
+        """
+        self.running = False
 
 
     async def main(self):
-        """ Подключение к сокетам
+        """ Подключение к потоку Binance через вебсокеты
+
+            Поток: 'wss://stream.binance.com:9443/ws/{self.symbol}@trade'
         """
         bm = BinanceSocketManager(client=CLIENT)
         ts = bm.trade_socket(self.symbol)
         async with ts as tscm:
-            while True:
+            while self.running:
                 res = await tscm.recv()
                 if res:
                     self.get_data(res)
+                await asyncio.sleep(0)
 
 
 # Создание объектов класса, где атрибут - элемент списка, и запуск созданных объектов многопоточно
 bots = [Datafarm(symbol) for symbol in symbol_list]
-try:
-    with ThreadPoolExecutor() as executor:
-        results = executor.map(asyncio.run, [bot.main() for bot in bots])
-except KeyboardInterrupt:
-    print('Stop')
+with ThreadPoolExecutor() as executor:
+    results = executor.map(asyncio.run, [bot.main() for bot in bots])
